997d75f178390576044a10b2965b84ba1e4cff0c
Lakhdar <Lakhdarbhl76@gmail.com>
2025-07-04 20:47:01 +0100

    Initialize and link project files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorButton = document.querySelector("#scissor");
+
+const playaChoice = document.querySelector(".playerChoice");
+const botChoice = document.querySelector(".computerChoice");
+const score = document.querySelector(".score");
+const roundRes = document.querySelector(".roundResult");
+const outcoem = document.querySelector(".outcome");
